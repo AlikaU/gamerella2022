d6b06f6cc678587f4c1a2ce79b2dd3cd2b9bc363
AlikaU <alika.utepova@gmail.com>
2022-12-03 20:38:05 -0500

    make player only die if he is not masked

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+
+// Import any other script files here, e.g.:
+// import * as myModule from "./mymodule.js";
+
+// Instance of Player
+let player = null;
+let previousMaskedStatus = false;
+
+runOnStartup(async runtime =>
+{
+	// Code to run on the loading screen.
+	// Note layouts, objects etc. are not yet available.
+	
+	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
+});
+
+async function OnBeforeProjectStart(runtime)
+{
+	// Code to run just before 'On start of layout' on
+	// the first layout. Loading has finished and initial
+	// instances are created and available to use here.
+	
+	runtime.addEventListener("tick", () => Tick(runtime));
+	
+	// get the player instance
+	player = runtime.objects.Player.getFirstInstance();
+}
+
+function Tick(runtime)
+{
+	if (player.masked){
+		console.log("masked");
+	} else {
+		console.log("not masked");
+	}
+	console.log(player.x)
+// 	if (player.Masked && previousMaskedStatus == false) {
+// 		console.log("I am grumpy and masked");
+// 		previousMaskedStatus = true;
+// 	} else if (!player.Masked && previousMaskedStatus == true) {
+// 		console.log("I am grumpy and not masked")
+// 		previousMaskedStatus = false;
+// 	}
+}
